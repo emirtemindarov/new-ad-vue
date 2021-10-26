b6929dee6a83ceaf24139e8ae6ecb8f94b17f863
Emir <temindarov.e.i.20@gmail.com>
2021-10-26 21:48:54 +0300

    готовый код Ðдля 2ю.1_2.2

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  
+  <v-app>
+  <v-navigation-drawer app></v-navigation-drawer> 
+  <v-app-bar app dark color="primary">
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+       <v-btn  text><v-icon left>mdi-cake-variant</v-icon>Link One</v-btn> 
+       <v-btn  text>Link Two</v-btn> 
+       <v-btn  text>Link Three</v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
+  <v-content>
+    <router-view></router-view> 
+  </v-content>
+  </v-app>
 </template>
-
-<script>
-export default {
-  
-}
-</script>
