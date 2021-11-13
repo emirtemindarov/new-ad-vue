ca1d4f4a1df3d912ef1946dac863a730df8c6783
Emir <temindarov.e.i.20@gmail.com>
2021-11-13 06:38:15 +0300

    Логин-форма и форма форма регистрации готовы

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -4,31 +4,34 @@
 			<v-flex xs12 sm8 md8>
 				<v-card class="elevetion-12">
 					<v-toolbar dark color="primary">
-						Login
+						<v-toolbar-title>Login</v-toolbar-title>
 					</v-toolbar>
 					<v-card-text>
-						<v-form v-model="valid" ref="form" validation>
+						<v-form v-model="valid" ref="form" lazy-validation>
 							<v-text-field
-								prepend-icon="mdi-account"
-								name="email"
-								label="Email"
-								type="email"
-								v-model="email"
-								:rules="emailRules">
+							prepend-icon="mdi-account" 
+							name="email" 
+							label="Email" 
+							type="email" 
+							v-model="email"
+							:rules="emailRules">
 							</v-text-field>
-							<v-text-field
-								prepend-icon="mdi-lock" 
-								name="password" 
-								label="Password" 
-								type="password" 
-								v-model="password"
-								:rules="passwordRules">
+							<v-text-field  
+							prepend-icon="mdi-lock" 
+							name="password" 
+							label="Password" 
+							type="password" 
+							v-model="password"
+							:rules="passwordRules">
 							</v-text-field>
-						</v-form>
+						</v-form> 
 					</v-card-text>
 					<v-card-actions>
 						<v-spacer></v-spacer>
-						<v-btn color="primary" @click="onSubmit" :disabled="!valid">
+						<v-btn 
+						color="primary"
+						@click="onSubmit"
+						:disabled="!valid">
 							Login
 						</v-btn>
 					</v-card-actions>	
@@ -37,7 +40,6 @@
 		</v-layout> 
 	</v-container>
 </template>
-
 
 
 
@@ -50,25 +52,24 @@ export default {
 			valid: false,
 			emailRules: [
 			v => !!v || 'E-mail is required',
-			v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+        	v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
 			],
 			passwordRules: [
-			v => !!v || 'Password is required',
-			v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters'
-			]
+	        v => !!v || 'Password is required',
+	        v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters'
+	        ]
 		} 	
 	},
 	methods: {
-							onSubmit(){
-								if (this.$refs.form.validate()) {
-									const user = {
-										email: this.email,
-										password: this.password
-									}
-									console.log(user)
-								}
-							}
-						}
-}
+		onSubmit() {
+			if (this.$refs.form.validate()) {
+				const user = {
+					email: this.email,
+					password: this.password
+				}
+				console.log(user)
+			}
+		}
+	}
+} 
 </script>
-
