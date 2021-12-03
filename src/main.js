2ae6b8bf768a5d16b5dbf050562e0cbcf65b53a4
Emir <temindarov.e.i.20@gmail.com>
2021-12-03 10:50:48 +0300

    Регистрация пользователей в Firebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router/index'
 import store from './store'
+import fb from 'firebase'
 
 Vue.use(Router)
 Vue.config.productionTip = false
@@ -12,5 +13,19 @@ new Vue({
   vuetify,
   render: h => h(App),
   router:router,
-  store
+  store,
+    created(){
+      const firebaseConfig = {
+        apiKey: "AIzaSyBJ1Bc2sTxB6QpcxmEZVnrkEcv-Kdf7pwo",
+        authDomain: "new-ad-vue.firebaseapp.com",
+        projectId: "new-ad-vue",
+        storageBucket: "new-ad-vue.appspot.com",
+        messagingSenderId: "768766598951",
+        appId: "1:768766598951:web:5798cd0439179bb858d302",
+        measurementId: "G-6VN5BY1ZHF"
+      };
+      // Initialize Firebase
+    fb.initializeApp(firebaseConfig);
+    fb.analytics();
+  }
 }).$mount('#app')
