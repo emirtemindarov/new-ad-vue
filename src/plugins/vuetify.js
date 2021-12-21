99f88466ad081fb0b26167fbf497c30d770c9f4b
Emir <temindarov.e.i.20@gmail.com>
2021-12-21 19:41:50 +0300

    Настроил тему в plugins/vuetify.js; случайно сломал database, когда делал пункт 20, из-за этого не могу увидеть результат работы

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,20 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: colors.indigo.darken1, // #E53935
+                secondary: colors.blue.lighten4, // #FFCDD2
+                accent: colors.teal.base, // #3F51B5
+                warning: colors.amber.darken1,
+                info: colors.lightBlue.base,
+                success: colors.green.darken1,
+            },
+        },
+    },    
 });
