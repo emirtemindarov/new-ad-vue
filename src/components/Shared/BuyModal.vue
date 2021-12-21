205d87c3716e8dcfc8276c5cf8f98cc439ae8f4e
Emir <temindarov.e.i.20@gmail.com>
2021-12-21 18:08:01 +0300

    Создание, верстка и логика компонента BuyModel.vue

---
+++ src/components/Shared/BuyModal.vue
@@ -0,0 +1,90 @@
+<template>
+    <v-dialog
+    v-model="modal"
+    width="400px"
+    >
+       <template v-slot:activator="{ on }"><v-btn 
+        v-on="on"
+        color="primary"
+        >
+        Buy</v-btn></template>
+        <v-card>
+            <v-container>
+                <v-layout row>
+                <v-flex xs>
+                <v-card-title>
+                <h1 class="text--primary">Do you want to buy it?</h1>
+                </v-card-title>
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex xs>
+                <v-card-text>
+                    <v-text-field
+                    name="name" 
+                    label="Your name" 
+                    type="text" 
+                    v-model="name" 
+                    >
+                    </v-text-field>
+                    <v-text-field
+                    name="phone" 
+                    label="Your phone" 
+                    type="text" 
+                    v-model="phone" 
+                    >
+                    </v-text-field>
+                </v-card-text>         			
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex xs>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn text @click="onCancel">Close</v-btn>
+                  <v-btn color="success" @click="onSave">Buy it!</v-btn>
+                </v-card-actions>
+                </v-flex>
+            </v-layout>
+        </v-container>
+        </v-card>
+    </v-dialog>
+</template>
+
+
+
+<script>
+	export default {
+    props: ['ad'],
+    data() {
+      return {
+        modal: false,
+        name: "",
+		phone: ""
+      }
+    },
+    methods: {
+      onCancel (){
+        this.name = ""
+		this.phone = ""
+        this.modal = false
+      },
+      onSave (){
+        if (this.name !== '' && this.phone !== '') {
+			this.$store.dispatch('createOrder', {
+                name: this.name,
+				phone: this.phone,
+				adId: this.ad.id,
+				ownerId: this.ad.ownerId
+			})
+			.finally(() => {
+				this.localLoading = false
+				this.name = ""
+				this.phone = ""
+				this.modal = false
+			})
+		}
+      }
+    }
+  }
+</script>
